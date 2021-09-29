d561e968df19c453077f6141f55a7df3b7b50920
Sixto Guerra <sixto.guerra1982@gmail.com>
2021-09-29 19:39:48 -0300

    iniciar sesion

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,21 @@
 class SessionsController < ApplicationController
+  include BCrypt
   def new
 
   end
 
   def create
+    @user = User.find_by_email(params[:email])
     byebug
+    # Password.create(params[:password])
+    # if user &&  
+    if @user.present? && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: 'Bienvenido'
+    else
+      session[:id] = nil
+      redirect_to root_path, notice: 'Acceso no Autorizado'
+    end
   end
 
   def destroy
